a804cce7cab3300df2dc6395955eb1b0edf76dc9
FernandoKain <fernandomanga@hotmail.com>
2024-06-26 21:16:29 -0300

    Métodos testados e funcionais
    Signed-off-by: FernandoKain <fernandomanga@hotmail.com>

---
+++ src/list/OperacoesBasicas/ListaTarefa.java
@@ -0,0 +1,23 @@
+package list;
+
+import list.OperacoesBasicas.Tarefa;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ListaTarefa {
+    //atributo
+    private List<Tarefa> tarefaList;
+
+    public ListaTarefa() {
+        this.tarefaList = new ArrayList<>();
+    }
+
+    public void adicionarTarefa(String descricao){
+        tarefaList.add(new Tarefa(descricao));
+    }
+
+    public void removerTarefa(String descricao){
+
+    }
+}
